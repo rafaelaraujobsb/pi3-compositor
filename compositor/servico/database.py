cfd6fd5995627e2a51988a0e731e0f460d7681f4
C1298058 Rafael Rodrigues Pereira de Araujo <c1298058@interno.bb.com.br>
2019-10-23 15:57:29 -0300

    database

--- compositor/servico/database.py
+++ compositor/servico/database.py
@@ -1,48 +1,54 @@
+import gridfs
 from pymongo import MongoClient
 
 
 from compositor.config import MONGO_DB, MONGO_HOST, MONGO_PWD, MONGO_USER
 
 class ModDatabase:
-    """ 
+    """
     Módulo mongo
     """
-    def __init__(self, db, user=None, password=None, host='localhost'): 
-        """ 
+    def __init__(self, db, user=None, password=None, host='localhost', port=27017):
+        """
         Faz a conexão com o db.
         """
         if user:
-            self.__client = MongoClient(f'mongodb+srv://{user}:{password}@{host}/{db}')
+            self.__client = MongoClient(f'mongodb://{user}:{password}@{host}:{port}/{db}')
         else:
             self.__client = MongoClient(f'mongodb://{host}:{port}/{db}')
 
-        self.__db = self.__client.get_database()   
+        self.__db = self.__client.get_database()
+        self.__fs = gridfs.GridFS(self.__db)
 
-    def get_names_collections(self):
-        """ 
+    def get_names_collections(self) -> list:
+        """
         Busca todas as collections.
+
+        :return: lista com o nome das collections.
+        :rtype: list
         """
         return self.__db.collection_names()
 
-    def get_document(self, collection, filter=None, visible=None, max=0, sort=[('_id', 1)]):
-        """ 
+    def get_document(self, collection: str, filter: dict = None, visible: dict = None, max: int = 0,
+                     sort: list = [('_id', 1)]) -> list:
+        """
         Busca todos os documentos encontrados.
 
         :param str collection: nome da collection
         :param dict filter: filtro da busca
         :param dict visible: colunas que serão mostradas ou não
-        :param int max: quantidade máxima de matches 
+        :param int max: quantidade máxima de matches
         :param list sort: | ordenação do resultado (ASCENDING = 1, DESCENDING = -1)
                           | Exemplo: [('_id', 1)]
 
         :returns: resultado da busca
         :rtype: list
-        """ 
+        """
 
-        return [ r for r in self.__db[collection].find(filter, visible).sort(sort).limit(max) ]
+        return [r for r in self.__db[collection].find(filter, visible).sort(sort).limit(max)]
 
-    def get_distinct(self, collection, column):
-        """ 
+    def get_distinct(self, collection: str, column: str, filter: dict = None) -> tuple:
+        """
         Busca todos os dados que são único.
 
         :param str collection: nome da collection
@@ -52,12 +58,16 @@ class ModDatabase:
         :rtype: tuple
         """
         unq = self.__db[collection].distinct(column)
-        count = [ self.__db[collection].find({column: r}).count() for r in unq ]
+
+        count = list()
+        for r in unq:
+            filter[column] = r
+            count.append(self.__db[collection].find(filter).count())
 
         return unq, count
 
-    def __next_id(self, collection):
-        """ 
+    def __next_id(self, collection: str) -> int:
+        """
         Faz a geração de id para substir o objectid.
 
         :param str collection: nome da collection
@@ -65,15 +75,18 @@ class ModDatabase:
         :returns: id sequencial gerado
         :rtype: int
         """
-        try:
-            _id = self.__db.seqs.find_and_modify({'_id': collection}, {'$inc': {'id': 1}}, upsert=True)['id']
-        except TypeError:
-            _id = self.__db.seqs.find_and_modify({'_id': collection}, {'$inc': {'id': 1}}, upsert=True)['id']
+
+        resultado = self.__db.seqs.find_one_and_update({'_id': collection}, {'$inc': {'id': 1}}, upsert=True)
+
+        if not resultado:
+            _id = 1
+        else:
+            _id = resultado['id'] + 1
 
         return _id
 
-    def set_document(self, collection, value, auto=False):
-        """ 
+    def set_document(self, collection: str, value: dict, auto: bool = False) -> int:
+        """
         Insere um documento.
 
         :param str collection: nome da collection
@@ -91,8 +104,8 @@ class ModDatabase:
 
         return self.__db[collection].insert_one(value).inserted_id
 
-    def update_document(self, collection, filter, value):
-        """ 
+    def update_document(self, collection: str, filter: dict, value: dict) -> int:
+        """
         Altera um ou mais documentos.
 
         :param str collection: nome da collection
@@ -106,18 +119,27 @@ class ModDatabase:
             `value` não precisa conter `$set`
         """
 
-        if '$push' not in value:
+        if '$push' not in value and '$inc' not in value:
             value = {'$set': value}
 
         result = self.__db[collection].update_many(filter, value)
 
         return result.modified_count
 
-    def len_collection(self, collection, filter=None):
+    def len_collection(self, collection: str, filter: dict = None) -> int:
+        """
+        Quando dados existem de acordo com o filtro
+
+        :param str collection: nome da collection
+        :param dict filter: filtro da busca
+
+        :returns: quantidade de registros
+        :rtype: int
+        """
         return self.__db[collection].find(filter).count()
 
-    def drop_collection(self, collection):
-        """ 
+    def drop_collection(self, collection: str) -> None:
+        """
         Deleta uma collection
 
         :param str collection: nome da collection
@@ -125,16 +147,45 @@ class ModDatabase:
         self.__db[collection].drop()
         self.__db.seqs.delete_one({'_id': collection})
 
+    def set_file(self, file: str, filename: str = None) -> str:
+        """
+        Salva um arquivo usando o GridFs.
+
+        :param file str: file pode ser uma string ou bytes.
+
+        :return: objectid do gridfs
+        :rtype: str
+        """
+        if type(file) == str:
+            file = file.encode()
+
+        return str(self.__fs.put(file, filename=filename))
+
+    def get_file(self, objectid: str) -> bytes:
+        """
+        Busca um arquivo salvo no GridFs.
+
+        :param objectid str: ObjectID do GridFS
+
+        :return: Documento salvo
+        :rtype: bytes
+        """
+        return self.__fs.get(ObjectId(objectid)).read()
+
+    def drop_database(self, db: str) -> None:
+        """
+        Deleta um databse
+
+        :param db str: nome do database
+        """
+        self.__client.drop_database(db)
+
     def __del__(self):
-        """ 
+        """
         Encerra a conexão quando o objeto é destruído.
         """
         self.__client.close()
 
-        self.__db.seqs.delete_one({'_id': collection})
-
-    def __del__(self):
-        self.__client.close()
 
 
 class Database(ModDatabase):
