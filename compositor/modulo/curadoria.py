c9c7c385a3808efc31e870cd1f7841066b442b66
Rafael Araujo <bsb.rafaelaraujo@gmail.com>
2019-10-23 18:53:21 -0300

    correcao porcentagem APi

--- compositor/modulo/curadoria.py
+++ compositor/modulo/curadoria.py
@@ -8,7 +8,7 @@ def lote_validacao(curador):
     db = Database()
     frases = db.get_document('curadoria', {'curador': curador, 'status': 0}, max=10)
     qtd = db.get_document('curador', {'_id': curador}, {'_id': 0})[0]
-    prc_feito = (qtd['curado'] / qtd['curar']) / 100
+    prc_feito = (qtd['curado'] / qtd['curar']) * 100
     # frases = DATABASE['curadoria']
     # qtd = DATABASE['curador'][0]
     # prc_feito = (qtd['curado'] / qtd['curar']) * 100
